c4476d25275d23669ed8b6478d744dbcfa55cad2
Kieran Gordon <kjg2000@hw.ac.uk>
2024-01-28 22:39:56 +0000

    Add CEF logging

--- server/app.py
+++ server/app.py
@@ -1,3 +1,5 @@
+import os
+from datetime import datetime
 from flask import Flask
 from flask_cors import CORS
 from flask_jwt_extended import JWTManager
@@ -10,21 +12,40 @@ from routes.user_endpoints import user_endpoints
 from routes.vm_endpoints import vm_endpoints
 from routes.admin_endpoints import admin_endpoints
 
-app = Flask(__name__)
-app.config.from_object(ApplicationConfig)
-CORS(app, supports_credentials=True)
-Bcrypt = Bcrypt(app)
-jwt = JWTManager(app)
-db.init_app(app)
-mail = Mail(app)
-migrate = Migrate(app, db)
+# Create Flask app
+app = Flask(__name__) # __name__ is the name of the current Python module
+app.config.from_object(ApplicationConfig) # Load config from config.py
+CORS(app, supports_credentials=True) # Enable CORS for all routes
+Bcrypt = Bcrypt(app) # Initialize Bcrypt for password hashing
+jwt = JWTManager(app) # Initialize JWT for authentication
+db.init_app(app) # Initialize database connection
+mail = Mail(app) # Initialize Mail for sending emails
+migrate = Migrate(app, db) # Initialize Migrate for database migrations
 
+# Create database tables
 with app.app_context():
     db.create_all()
 
+# Register blueprints
 app.register_blueprint(user_endpoints)
 app.register_blueprint(vm_endpoints)
 app.register_blueprint(admin_endpoints)
+
+# Create logs/ directory if it doesn't exist
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
+# Create iso/ directory if it doesn't exist
+if not os.path.exists('iso'):
+    os.makedirs('iso')
+
+# Create iso/logos/ directory if it doesn't exist
+if not os.path.exists('iso/logos'):
+    os.makedirs('iso/logos')
+
+# Create iso/index.json if it doesn't exist
+if not os.path.exists('iso/index.json'):
+    open('iso/index.json', 'a').close()
 
 if __name__ == '__main__':
     app.run(debug=True)
