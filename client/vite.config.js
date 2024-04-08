5905357e1d282b69d1b59b26d6d7ab7b5086c3f4
Kieran Gordon <kjg2000@hw.ac.uk>
2024-04-08 02:33:31 +0100

    Add better ESLint support

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
   build: {
     outDir: 'build',
   },
-  plugins: [react()],
+  plugins: [react(), eslint()]
 })
 
