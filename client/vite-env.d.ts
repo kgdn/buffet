0e18030161cdd4ee895a8b92d712cbbb5bc2a873
Kieran Gordon <kieran.gordon28@gmail.com>
2024-12-16 00:33:31 +0000

    Update Vite configuration and dependencies; enable source maps and adjust base URL

--- client/vite-env.d.ts
+++ client/vite-env.d.ts
@@ -4,7 +4,7 @@ interface ImportMetaEnv {
     readonly VITE_APP_TITLE: string;
     readonly BROWSER: string;
     readonly GENERATE_SOURCEMAP: string;
-    readonly VITE_BASE_URL: string;
+    readonly BASE_URL: string;
     readonly VITE_BASE_PORT: string;
     readonly VITE_MAX_VM_COUNT: string;
 }
